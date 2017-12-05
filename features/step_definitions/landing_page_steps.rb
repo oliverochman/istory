a9ad2394053cd223f3005d907b68e5814fffa80e
oliver_ochman <oliver.ochman@hotmail.com>
2017-12-05 19:46:11 +0100

    adds navbar

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -9,3 +9,7 @@ end
 Then("I should see {string} element") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
+
+Then("I should see {string} button") do |navbar_link|
+  expect(page).to have_content navbar_link
+end
