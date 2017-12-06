4eb273cf3cabc1a76e6a919ce7449a4831f71ed7
Lisa Eriksson <lisa.c.eriksson@gmail.com>
2017-12-06 11:19:45 +0100

    Landing page (#4)
    * adds landing page view
    
    * adds feature file
    
    * adds navbar
    
    * Completes requirements for basic landing page
    
    * Adds routes, new syntax
    
    * Adds routes
    
    * Rev li class
    
    * x out links
    
    * Adds js include and stylesheet

---
+++ features/step_definitions/landing_page_steps.rb
@@ -0,0 +1,19 @@
+Given("I am on the landing page") do
+  visit '/'
+end
+
+Then("I should see {string}") do |content|
+  expect(page).to have_content content
+end
+
+Then("I should see {string} element") do |text|
+  expect(page).to have_content text
+end
+
+Then("I should see {string} button") do |navbar_link|
+  expect(page).to have_content navbar_link
+end
+
+Then("show me the page") do
+  save_and_open_page
+end
